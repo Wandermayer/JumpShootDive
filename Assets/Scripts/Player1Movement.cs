4709e49c368c575cdcaf2fd3608cfdf83f3e2eff
Wandermayer <chapmanz1700@my.uwstout.edu>
2015-12-13 05:25:37 -0600

    First
    The start

---
+++ Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Movement : MonoBehaviour {
+
+
+	public float jumpHeight;
+	public Rigidbody myRigidbody;
+	bool grounded = true;
+	bool doubleJump = false;
+	public Transform projectileSpawn;
+	public GameObject projectile;
+	bool ammo;
+	bool dive = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && ammo && !grounded) {
+			Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+			ammo = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow) ) {
+			if(grounded){
+			myRigidbody.AddForce(0, jumpHeight, 0);
+				doubleJump = true;
+				ammo = true;
+			}else{
+				if(doubleJump){
+					dive = true;
+					doubleJump = false;
+					myRigidbody.velocity = Vector3.zero;
+					myRigidbody.AddForce(0, jumpHeight, 0);
+
+				}
+				else{
+					if(dive){
+						myRigidbody.velocity = Vector3.zero;
+						myRigidbody.AddForce(0, -jumpHeight, 0);
+					}
+				}
+			}
+		}
+
+		if (transform.position.y > 1) {
+			grounded = false;
+		} else {
+			grounded = true;
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public bool facingRight;
+	float velocity = -1;
+	float lifeTime = 3f;
+
+	// Use this for initialization
+	void Start () {
+		if (facingRight) {
+			velocity = -velocity;
+		}
+		Destroy (gameObject, lifeTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3 (transform.position.x + velocity, transform.position.y, transform.position.z);
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player1") {
+			Debug.Log("Player2 wins!");
+		}
+		else{
+			if(other.tag == "Player2"){
+				Debug.Log ("Player1 Wins");
+			}
+		}
+	}
+}
+
