d4754a3f71e3a265004eebcfef5cadae7b77ea29
AntTAKEOVER <vedantpatwari01@gmail.com>
2015-12-14 09:05:09 +0530

    Animation 2
    -

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	bool hasFinshedAnim;
+	public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
