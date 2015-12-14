62b979eb917f115ec653c924707bdc1682b56377
AntTAKEOVER <vedantpatwari01@gmail.com>
2015-12-14 09:11:59 +0530

    Duplicated Scene for Anims
    —

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,12 +9,16 @@ public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Test2_Anim")) {
+			Debug.Log ("Works!");
+		} else {
+			Debug.LogWarning ("Does't work!");
+		}
 	}
 
 }
